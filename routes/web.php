5ee001c1ddbaa441c1c8248f70962727face8f35
lesteramores <lestervergaraamores@gmail.com>
2025-05-04 22:00:09 +0800

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -11,6 +12,14 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+});
+
+
+Route::prefix('admin')->middleware(['auth', 'verified'])->group(function(){
+    Route::resource('/admins', AdminController::class);
+    Route::patch('/admins/restore', [AdminController::class, 'restore'])->name('restore');
+    Route::post('/admins/multi-delete', [AdminController::class, 'multiDelete'])->name('multi-delete');
+    Route::post('/admins/multi-restore', [AdminController::class, 'multiRestore'])->name('multi-restore');
 });
 
 require __DIR__.'/settings.php';
